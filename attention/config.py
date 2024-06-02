4505f852a33e7b20502f10642e7cc1d37a10f7bd
dhanesh <dhanesh.padm@gmail.com>
2024-06-02 16:34:12 +0530

    updating changes

--- attention/config.py
+++ attention/config.py
@@ -3,13 +3,13 @@ from pathlib import Path
 def get_config():
     return {
         "batch_size": 16,
-        "num_epochs": 60,
+        "num_epochs": 10,
         "lr": 10**-4,
         "seq_len": 350,
         "d_model": 512,
         "datasource": 'opus_books',
         "lang_src": "en",
-        "lang_tgt": "it",
+        "lang_tgt": "fr",
         "model_folder": "weights",
         "model_basename": "tmodel_",
         "preload": "latest",
